90a760a250de705307b41e2c3be7b089f0b088ec
hortuadev <hortuadev@gmail.com>
2025-11-05 10:43:05 +0000

    fix(Header): agregado el componente a la estrucutura principal

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import Header from "./Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header />
     </View>
   );
 }
