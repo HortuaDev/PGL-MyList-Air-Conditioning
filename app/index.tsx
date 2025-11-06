2d6df0a0af2fcd87b6347f62f4038bd12a65e518
hortuadev <hortuadev@gmail.com>
2025-11-06 14:19:57 +0000

    feat(App): agregar componente NavBar y establecer color de fondo

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./Header";
+import NavBar from "./NavBar";
+import Color from "../utils/Color";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <NavBar />
     </View>
   );
 }
@@ -12,5 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: Color.background_primary,
   },
 });
