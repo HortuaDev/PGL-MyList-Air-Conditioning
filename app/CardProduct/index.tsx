9f1d765d285b8fc4373ddd5a3a53811079a741a6
hortuadev <hortuadev@gmail.com>
2025-11-06 14:33:52 +0000

    feat(CardProduct): agregar componente CardProduct con estructura básica

---
+++ app/CardProduct/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CardProduct = () => {
+  return (
+    <View>
+      <Text>CardProduct</Text>
+    </View>
+  );
+};
+
+export default CardProduct;
+
+const styles = StyleSheet.create({});
