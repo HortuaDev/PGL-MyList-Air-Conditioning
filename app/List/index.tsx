331cd90a39b8929104ec08b4b5e1de459d8b7701
hortuadev <hortuadev@gmail.com>
2025-11-06 14:27:54 +0000

    feat(List): agregar ScrollView y texto en el componente List

--- app/List/index.tsx
+++ app/List/index.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const List = () => {
   return (
-    <View>
-      <Text>List</Text>
+    <View style={styles.listContainer}>
+      <ScrollView style={styles.scrollContainer}></ScrollView>
     </View>
   );
 };
 
 export default List;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listContainer: { flex: 10 },
+  scrollContainer: {},
+});
