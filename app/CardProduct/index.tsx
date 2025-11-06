f21203da545c9b475fc93ad7bbc5e921060f9023
hortuadev <hortuadev@gmail.com>
2025-11-06 14:54:59 +0000

    feat(CardProduct): implementar el componente CardProduct con propiedades y estilos básicos

--- app/CardProduct/index.tsx
+++ app/CardProduct/index.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Product } from "../../types/CardProduct";
+("../../types/CardProduct");
 
-const CardProduct = () => {
+const CardProduct = ({ name, price, image, checked, deleted }: Product) => {
   return (
-    <View>
-      <Text>CardProduct</Text>
+    <View style={styles.cardProductContainer}>
+      <View style={styles.infoContainer}>
+        <View style={styles.nameContainer}>
+          <Text>{name}</Text>
+        </View>
+        <View style={styles.descContainer}>
+          <View style={styles.imgContainer}>
+            <Image source={image} />
+          </View>
+          <View style={styles.priceContainer}>
+            <Text>{price}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.buttonsConatiner}>
+        <View style={styles.checkButton}></View>
+        {deleted && <View style={styles.deleteButton}></View>}
+      </View>
     </View>
   );
 };
 
 export default CardProduct;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardProductContainer: {},
+  infoContainer: {},
+  nameContainer: {},
+  descContainer: {},
+  imgContainer: {},
+  priceContainer: {},
+  buttonsConatiner: {},
+  checkButton: {},
+  deleteButton: {},
+});
